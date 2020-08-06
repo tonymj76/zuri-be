8f708dcb4985f6dc03a6a7791a6d6486cc04d938
youngGaf <youngabuhafiz@gmail.com>
2020-08-06 15:28:48 +0100

    feat: superadmin delete an admin and get all admins

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
-const { login, logout, addAdmin } = require('../controller/adminController');
+const {
+  login,
+  logout,
+  addAdmin,
+  deleteAdmin,
+  getAllAdmins
+} = require('../controller/adminController');
 const {
   topAnalytics,
   internMentorTrackStats
@@ -12,6 +18,8 @@ const router = express.Router();
 router.post('/auth', login);
 router.get('/logout', logout);
 router.post('/superadmin/create/admin', addAdmin);
+router.delete('/superadmin/delete/:adminId', deleteAdmin);
+router.get('/superadmin/all/admins', getAllAdmins);
 
 router.get('/analytics/toplevel', topAnalytics);
 // router.get("/analytics/lowerlevel/intern-mentor-status")
