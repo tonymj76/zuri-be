578ceae82c2e5caff7b81c03f82f47f425399a68
ceofred <johnsonmessilo19@gmail.com>
2020-08-06 16:27:50 +0100

    using single quote

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-require("dotenv").config();
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
-const path = require("path");
+require('dotenv').config();
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const path = require('path');
 
-const adminRouter = require("./routes/adminRoutes");
-const zuriInternshipRouter = require("./routes/zuriInternshipRoutes");
-const contactRouter = require("./routes/contactRoutes");
-const zuriTrainingRouter = require("./routes/zuriTrainingRoutes");
-const { handleError } = require("./utils/error");
+const adminRouter = require('./routes/adminRoutes');
+const zuriInternshipRouter = require('./routes/zuriInternshipRoutes');
+const contactRouter = require('./routes/contactRoutes');
+const zuriTrainingRouter = require('./routes/zuriTrainingRoutes');
+const { handleError } = require('./utils/error');
 
 const app = express();
 
 app.use(cors());
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/cvDir", express.static(path.join(__dirname, "cvDir")));
+app.use('/cvDir', express.static(path.join(__dirname, 'cvDir')));
 
-app.use("/api/v1", adminRouter);
-app.use("/api/v1/internship", zuriInternshipRouter);
-app.use("/api/v1/training", zuriTrainingRouter);
-app.use("/api/v1", contactRouter);
+app.use('/api/v1', adminRouter);
+app.use('/api/v1/internship', zuriInternshipRouter);
+app.use('/api/v1/training', zuriTrainingRouter);
+app.use('/api/v1', contactRouter);
 
 // Express error middleware
 app.use((err, req, res, next) => next(handleError(res, err)));
 
 // Unknown endpoints middleware
-app.use("*", (req, res) => {
+app.use('*', (req, res) => {
   const url = req.originalUrl;
   res.status(404).send({
-    status: "error",
-    message: `Oops. ${req.method} ${url} not found on this website`,
+    status: 'error',
+    message: `Oops. ${req.method} ${url} not found on this website`
   });
 });
 
