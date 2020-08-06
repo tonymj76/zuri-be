455b6dd518eaf3f65a0b4a51a43d4d2f6d18879d
youngGaf <youngabuhafiz@gmail.com>
2020-08-06 12:20:45 +0100

    chore: upstream updates

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
@@ -9,6 +8,7 @@ const adminRouter = require('./routes/adminRoutes');
 const mentorRouter = require('./routes/mentorRoutes');
 const contactRouter = require('./routes/contactRoutes');
 const zuriTrainingRoute = require('./routes/zuriTrainingRoute');
+const zuriInternRoute = require('./routes/zuriInternRoutes');
 const { handleError } = require('./utils/error');
 
 const app = express();
@@ -23,6 +23,7 @@ app.use('/api/v1', adminRouter);
 app.use('/api/v1', mentorRouter);
 app.use('/api/v1', zuriTrainingRoute);
 app.use('/api/v1', contactRouter);
+app.use('/api/v1', zuriInternRoute);
 
 // Express error middleware
 app.use((err, req, res, next) => next(handleError(res, err)));
@@ -37,45 +38,3 @@ app.use('*', (req, res) => {
 });
 
 module.exports = app;
-=======
-require("dotenv").config();
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
-const path = require("path");
-
-const apiRouter = require("./routes/adminRoutes");
-const mentorRouter = require("./routes/mentorRoutes");
-const contactRouter = require("./routes/contactRoutes");
-const zuriTrainingRoute = require("./routes/zuriTrainingRoute");
-const zuriInternRoute = require("./routes/zuriInternRoutes");
-const { handleError } = require("./utils/error");
-
-const app = express();
-
-app.use(cors());
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/cvDir", express.static(path.join(__dirname, "cvDir")));
-
-app.use("/api/v1", apiRouter);
-app.use("/api/v1", mentorRouter);
-app.use("/api/v1", zuriTrainingRoute);
-app.use("/api/v1", contactRouter);
-app.use("/api/v1", zuriInternRoute)
-
-// Express error middleware
-app.use((err, req, res, next) => next(handleError(res, err)));
-
-// Unknown endpoints middleware
-app.use("*", (req, res) => {
-  const url = req.originalUrl;
-  res.status(404).send({
-    status: "error",
-    message: `Oops. ${req.method} ${url} not found on this website`,
-  });
-});
-
-module.exports = app;
->>>>>>> 67e4c9b72d197591f20ac102fdda2ee24b6df9a6
