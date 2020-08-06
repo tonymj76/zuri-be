026aefd8cce619640c621a1bc331eca38d5d8d51
tonymj76 <demeyortj@gmail.com>
2020-08-06 01:22:36 +0100

    admin login

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,99 +1,75 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable new-cap */
 /* eslint-disable no-console */
 const { isEmpty, isEmail } = require('validator');
-const { body, validationResult } = require('express-validator');
+// const { body } = require('express-validator');
 const bcrypt = require('bcryptjs');
-const Admins = require('../models/AdminLogin');
+const jwt = require('jsonwebtoken');
+const Admins = require('../models/Admin');
+const { JWTKey } = require('../config');
+const { responseHandler } = require('../utils/responseHandler');
 
-const newAdminValidationRules = () => [
-  body('firstName').isString(),
-  body('lastName').isString(),
-  body('email').isEmail(),
-  body('adminpassword').isLength({ min: 5 }),
-  body('role').isString()
-];
+// const newAdminValidationRules = () => [
+//   body('firstName').isString(),
+//   body('lastName').isString(),
+//   body('email').isEmail(),
+//   body('adminpassword').isLength({ min: 5 }),
+//   body('category').isString(),
+//   body('role').isString()
+// ];
 
 // Admin Login
 const login = (req, res) => {
   const { email, password } = req.body;
   if (isEmpty(email) || isEmpty(password)) {
-    req.flash('error', 'All fields are required');
-    res.redirect('/login');
+    responseHandler(res, 'unauthorized access', 401);
   }
   if (!isEmail(email)) {
-    req.flash('error', 'Please enter a valid email');
-    res.redirect('/login');
+    responseHandler(res, 'Please enter a valid email');
   }
-
   Admins.findOne({ email }).then((admin) => {
     if (!admin) {
-      req.flash('error', 'Email does not exist in our record');
-      res.redirect('/login');
-    } else {
-      bcrypt.compare(password, admin.password, (err, result) => {
-        if (result) {
-          req.session.auth = true;
-          req.session.lastVisited = req.session.visited || Date.now();
-          req.session.visited = Date.now();
-          req.session.name = admin.name;
-          req.session.email = admin.email;
-          req.session.role = admin.role;
-          req.flash('success', 'Welcome back, You\'re logged in');
-          res.redirect('/dashboard');
-        } else {
-          req.flash('error', 'Invalid Password');
-          res.redirect('/login');
-        }
-      });
+      responseHandler(res, 'Email does not exist in our record');
+      return;
     }
+    bcrypt.compare(password, admin.password).then(
+      (valid) => {
+        if (!valid) {
+          responseHandler(res, 'Please enter a valid password');
+        }
+        const token = jwt.sign(
+          { adminId: admin._id },
+          JWTKey,
+          { expiresIn: '24h' }
+        );
+        const { name, role, category } = admin;
+        responseHandler(res, 'token gen and successful login', 200, true, {
+          name,
+          email,
+          role,
+          category,
+          authorization: { token }
+        });
+      }
+    ).catch(
+      (err) => {
+        res.status(501).json({
+          error: err
+        });
+      }
+    );
   }).catch((err) => {
-    res.json('Errr');
+    res.status(500).json({
+      error: err
+    });
   });
 };
 
 const logout = (req, res) => {
-  req.session.auth = false;
-  req.session.email = null;
-  req.session.name = null;
-  req.session.role = null;
-  res.redirect('/');
-};
-
-const createAdmin = (req, res) => {
-  const {
-    firstname, lastname, email, role, adminpassword
-  } = req.body;
-  const errors = validationResult(req);
-  const err = errors.array();
-  err.forEach((er) => {
-    const message = `${er.msg} in ${er.param}`;
-    req.flash('error', message);
-  });
-  Admins.findOne({ email }).then(
-    (newAdmin) => {
-      if (!newAdmin) {
-        req.flash('error', `user with this ${email} already exits`);
-        return;
-      }
-
-      bcrypt.hash(adminpassword, 10).then((hash) => {
-        const admin = new Admins({
-          name: `${lastname} ${firstname}`,
-          password: hash,
-          email,
-          role: role === 'admin' ? role : 'superAdmin'
-        });
-        admin.save().then(
-          () => { res.flash('success', `${role} Created successfully`); }
-        ).catch((error) => { req.flash('error', error); });
-      });
-    }
-  ).catch((errs) => { req.json(errs); });
+  responseHandler(res, 'No Logout');
 };
 
 module.exports = {
   login,
-  logout,
-  createAdmin,
-  newAdminValidationRules
+  logout
 };
